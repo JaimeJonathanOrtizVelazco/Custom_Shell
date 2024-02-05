e767cf55a03b52fbbc333ea0b5b0e5370b117e1e
Jaime Jonathan Ortiz Velazco <jonathan.ortiz.velazco@gmail.com>
2024-02-04 23:04:52 -0600

    Adding kitty Config files, .zshrc and creation installer

---
+++ installer.sh
@@ -0,0 +1,6 @@
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/zsh/plugins/zsh-autosuggestions
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting /usr/share/zsh/plugins/zsh-syntax-highlighting
